edb644349534c9a060f29350002fae3b0f41bdea
aydlokojhb024 <aydlokojhb024@student.wethinkcode.co.za>
2025-07-06 20:42:19 +0200

    added a simple server class that listen for client connection

---
+++ src/main/java/org/server/Server.java
@@ -0,0 +1,21 @@
+package org.server;
+
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            // Creating a new serverSocket object
+            ServerSocket serverSocket = new ServerSocket(4221);
+
+            serverSocket.setReuseAddress(true);
+
+            serverSocket.accept();
+            System.out.println("accepted new connection");
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
+}
