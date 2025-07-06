fae8141297b524d1fee1ffafa1b0896a0a79ce65
aydlokojhb024 <aydlokojhb024@student.wethinkcode.co.za>
2025-07-06 21:01:22 +0200

    removed the server constructor

--- src/main/java/org/server/Server.java
+++ src/main/java/org/server/Server.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.net.ServerSocket;
 
 public class Server {
-    public Server(int testPort) {
-    }
 
     public static void main(String[] args) {
         try {
